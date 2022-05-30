08e2bfc62c4fbe8c1d8c458218cd71d517a787bd
tomorrow9913 <tomorrow9913@gmail.com>
2022-05-30 13:03:07 +0900

    feat: 스레드가 메시지에 같이 생성되도록 변경

--- commands/bamboo.js
+++ commands/bamboo.js
@@ -2,8 +2,6 @@ const { MessageActionRow, Modal, TextInputComponent } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
-function getRandomInt(min, max) {return Math.floor(Math.random() * (max - min)) + min;}
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('bamboo')
@@ -24,12 +22,6 @@ module.exports = {
             .setLabel("내용을 입력해주세요")
             // Paragraph means multiple lines of text.
             .setStyle('PARAGRAPH');
-        const authorInput = new TextInputComponent()
-            .setCustomId('authorInput')
-            // The label is the prompt the user sees for this input
-            .setLabel("익명은 빈칸 실명은 아무 값이나 넣어주세요")
-            // Short means only a single line of text
-            .setStyle('SHORT');
         // An action row only holds one text input,
         // so you need one action row per text input.
         const firstActionRow = new MessageActionRow().addComponents(titleInput);
@@ -38,32 +30,31 @@ module.exports = {
         // Add inputs to the modal
         modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
         // Show the modal to the user
-        await interaction.showModal(modal);
+        return await interaction.showModal(modal);
     },
     async process(interaction) {
         const tilte = interaction.fields.getTextInputValue('tilteInput');
         const contens = interaction.fields.getTextInputValue('contensInput');
-        const author = interaction.fields.getTextInputValue('authorInput');
-
-        // inside a command, event listener, etc.
-        const exampleEmbed = new MessageEmbed()
-        .setColor('#'+getRandomInt(0,255).toString(16)+getRandomInt(0,255).toString(16)+getRandomInt(0,255).toString(16))
+        
+        const bambooEmbed = new MessageEmbed()
+        .setColor('#98c379')
         .setTitle(`${tilte}`)
-        .setAuthor({ name: '코딩야학 대나무숲', iconURL: `https://cdn.discordapp.com/icons/${interaction.channelId}/5aeb8b9b13d13c046bebb46524b77e29.webp?size=96`, url: 'https://cdn.discordapp.com/icons/336499288001478656/5aeb8b9b13d13c046bebb46524b77e29.webp?size=96' })
+        .setAuthor({ name: '코딩야학 대나무숲', iconURL: interaction.guild.bannerURL(), url: 'https://cdn.discordapp.com/icons/336499288001478656/5aeb8b9b13d13c046bebb46524b77e29.webp?size=96' })
         .setDescription(`${contens}`)
         .setTimestamp()
         .setFooter({ 
-            text: `${author=='' ? "익명" : interaction.user.username}`, 
-            iconURL: `${author=="" ? `https://cdn.discordapp.com/icons/${interaction.channelId}/5aeb8b9b13d13c046bebb46524b77e29.webp?size=96` : `https://cdn.discordapp.com/icons/${interaction.user.id}/5aeb8b9b13d13c046bebb46524b77e29.webp?size=96`}`
+            text: "익명", 
+            iconURL: interaction.guild.bannerURL()
          });
         
-        interaction.channel.send({ embeds: [exampleEmbed] })
-        .then(()=>{
-           interaction.channel.threads.create({
-                name: `${tilte}`,
-                autoArchiveDuration: 60,
-                reason: '대나무 숲 질문 생성',
-            });
+        interaction.reply({ content: `대나무 숲에 새로운 외침이 나타났습니다.`, embeds: [bambooEmbed] });
+
+        const message = await interaction.fetchReply();
+        
+        return await message.startThread({
+            name: `${tilte}`,
+            autoArchiveDuration: 60,
+            reason: '대나무 숲 질문 생성',
         });
     }
     

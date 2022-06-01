13b06ac52e656fa02538117f86fb1cb8d7fd7351
tomorrow9913 <tomorrow9913@gmail.com>
2022-06-01 12:36:51 +0900

    style : embedMSG.js 가독성 향상 수정

--- structures/embedMsg.js
+++ structures/embedMsg.js
@@ -10,37 +10,19 @@ module.exports = {
         .setTimestamp();
         return [embed];
     },
+
     bambooInfoEmbed(interaction, tilte, contens) {
         const embed = new MessageEmbed()
         .setColor('#98c379')
         .setTitle(`${tilte}`)
-        .setAuthor({ name: '코딩야학 대나무숲', iconURL: interaction.guild.iconURL(), url: 'https://cdn.discordapp.com/icons/336499288001478656/5aeb8b9b13d13c046bebb46524b77e29.webp?size=96' })
+        .setAuthor({ name: '코딩야학 대나무숲', iconURL: interaction.guild.iconURL() })
         .setDescription(`${contens}`)
         .setTimestamp()
         .setFooter({ 
             text: "익명", 
-            iconURL: interaction.guild.bannerURL()
+            iconURL: interaction.guild.iconURL()
          });
 
          return [embed];
     },
-    simpleInfoEmbed(){
-        return new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({ name: '유저 정보', iconURL: interaction.guild.bannerURL()})
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-    }
 };
